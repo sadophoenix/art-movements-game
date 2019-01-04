7a7d82281fc36a04ad1aefb814ae90bc27d941f7
David Santogidis <s.david19193@gmail.com>
2019-01-04 23:39:54 +0200

    Fixed indentations

--- src/Cell.js
+++ src/Cell.js
@@ -5,47 +5,47 @@ import { DragSource, DropTarget } from 'react-dnd';
 import Piece from './Piece';
 
 const Cell = (props) => {
-  const { image, size, level, position, connectDropTarget, isOver } = props;
-  const side = (size / level);
-  const x = (position % level) * side;
-  const y = Math.floor(position / level) * side;
+    const { image, size, level, position, connectDropTarget, isOver } = props;
+    const side = (size / level);
+    const x = (position % level) * side;
+    const y = Math.floor(position / level) * side;
 
-  return connectDropTarget(
-    <div className='piece'>
-      <Piece
-        position={position}
-        image={image}
-        size={size}
-        side={side}
-        x={x}
-        y={y}
-        isOver={isOver}
-      />
+    return connectDropTarget(
+        <div className='piece'>
+            <Piece
+                position={position}
+                image={image}
+                size={size}
+                side={side}
+                x={x}
+                y={y}
+                isOver={isOver}
+            />
 
-      <style>{`
+            <style>{`
         .piece:hover {
           opacity: 0.8;
         }
       `}</style>
-    </div>
-  );
+        </div>
+    );
 };
 
 const squareTarget = {
-  drop(props, monitor) {
-    const item = monitor.getItem();
-    const sourcePosition = item.position;
-    const dropPosition = props.position;
+    drop(props, monitor) {
+        const item = monitor.getItem();
+        const sourcePosition = item.position;
+        const dropPosition = props.position;
 
-    props.onSwap(sourcePosition, dropPosition);
-  }
+        props.onSwap(sourcePosition, dropPosition);
+    }
 };
 
 function collect(connect, monitor) {
-  return {
-    connectDropTarget: connect.dropTarget(),
-    isOver: monitor.isOver()
-  };
+    return {
+        connectDropTarget: connect.dropTarget(),
+        isOver: monitor.isOver()
+    };
 }
 
 export default DropTarget('piece', squareTarget, collect)(Cell);
