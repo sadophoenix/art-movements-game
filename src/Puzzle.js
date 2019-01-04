18a23369de837d0dca50fe82b356051a2e0d2e27
David Santogidis <s.david19193@gmail.com>
2019-01-05 01:38:52 +0200

    Indentations to 4 spaces

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -13,128 +13,128 @@ import { DragDropContext } from 'react-dnd';
  * @return {Array}
  */
 function shuffle(a) {
-  const b = a.slice();
+    const b = a.slice();
 
-  for (let i = b.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [b[i], b[j]] = [b[j], b[i]];
-  }
+    for (let i = b.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [b[i], b[j]] = [b[j], b[i]];
+    }
 
-  return b;
+    return b;
 }
 
 class Puzzle extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    const { level } = props;
-    const cells = 2 * level * level;
+        const { level } = props;
+        const cells = 2 * level * level;
 
-    this.state = { positions: [...Array(cells).keys()] };
-  }
-
-  componentDidMount() {
-    const { positions } = this.state;
-
-    this.setState({ positions: shuffle(positions) });
-  }
-
-  onSwap(sourcePosition, dropPosition) {
-    const oldPositions = this.state.positions.slice();
-    const newPositions = [];
-    let done = true;
-    let p = 0;
-
-    for (let i in oldPositions) {
-      let value = oldPositions[i];
-      let newValue = value;
-
-      if (value === sourcePosition) {
-        newValue = dropPosition;
-      } else if (value === dropPosition) {
-        newValue = sourcePosition;
-      }
-
-      newPositions.push(newValue);
-
-      if (newValue !== p) {
-        done = false;
-      }
-
-      p = p + 1;
+        this.state = { positions: [...Array(cells).keys()] };
     }
 
-    this.setState({ positions: newPositions });
+    componentDidMount() {
+        const { positions } = this.state;
 
-    if(done) {
-      this.props.onDone();
+        this.setState({ positions: shuffle(positions) });
     }
-  }
 
-  renderSquares() {
-    const { image, image2, size, level } = this.props;
-    const { positions } = this.state;
+    onSwap(sourcePosition, dropPosition) {
+        const oldPositions = this.state.positions.slice();
+        const newPositions = [];
+        let done = true;
+        let p = 0;
 
-    const squares = positions.map((i) => {
-      // If the quotient of i/level is even, then use first image, else the second
-      const q = Math.floor(i / level);
-      if (q % 2 === 0) {
+        for (let i in oldPositions) {
+            let value = oldPositions[i];
+            let newValue = value;
+
+            if (value === sourcePosition) {
+                newValue = dropPosition;
+            } else if (value === dropPosition) {
+                newValue = sourcePosition;
+            }
+
+            newPositions.push(newValue);
+
+            if (newValue !== p) {
+                done = false;
+            }
+
+            p = p + 1;
+        }
+
+        this.setState({ positions: newPositions });
+
+        if(done) {
+            this.props.onDone();
+        }
+    }
+
+    renderSquares() {
+        const { image, image2, size, level } = this.props;
+        const { positions } = this.state;
+
+        const squares = positions.map((i) => {
+            // If the quotient of i/level is even, then use first image, else the second
+            const q = Math.floor(i / level);
+            if (q % 2 === 0) {
+                return (
+                    <Cell
+                        key={i}
+                        size={size}
+                        image={image}
+                        level={level}
+                        position={i}
+                        onSwap={this.onSwap.bind(this)}
+                    />
+                );
+            } else {
+                return (
+                    <Cell
+                        key={i}
+                        size={size}
+                        image={image2}
+                        level={level}
+                        position={i}
+                        onSwap={this.onSwap.bind(this)}
+                    />
+                );
+            }
+        })
+
+        return squares;
+    }
+
+    render() {
+        const { size } = this.props;
+
         return (
-            <Cell
-                key={i}
-                size={size}
-                image={image}
-                level={level}
-                position={i}
-                onSwap={this.onSwap.bind(this)}
-            />
+            <div
+                style={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    padding: 0,
+                    width: `${2*size}px`,
+                    height: `${size}px`
+                }}>
+                {this.renderSquares()}
+            </div>
         );
-      } else {
-        return (
-            <Cell
-                key={i}
-                size={size}
-                image={image2}
-                level={level}
-                position={i}
-                onSwap={this.onSwap.bind(this)}
-            />
-        );
-      }
-    })
-
-    return squares;
-  }
-
-  render() {
-    const { size } = this.props;
-
-    return (
-      <div
-        style={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          padding: 0,
-          width: `${2*size}px`,
-          height: `${size}px`
-        }}>
-        {this.renderSquares()}
-      </div>
-    );
-  }
+    }
 };
 
 Puzzle.propTypes = {
-  image: PropTypes.string.isRequired,
-  size: PropTypes.number,
-  level: PropTypes.number,
-  onDone: PropTypes.func,
+    image: PropTypes.string.isRequired,
+    size: PropTypes.number,
+    level: PropTypes.number,
+    onDone: PropTypes.func,
 };
 
 Puzzle.defaultProps = {
-  size: 300,
-  level: 3,
-  onDone: () => {},
+    size: 300,
+    level: 3,
+    onDone: () => {},
 };
 
 export default DragDropContext(HTML5Backend)(Puzzle);
