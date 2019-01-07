572a1e3d247639857c4ed3f7eb5ba43ca5d40ece
David Santogidis <s.david19193@gmail.com>
2019-01-07 17:26:57 +0200

    delete junk files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import Puzzle from './Puzzle';
 import * as serviceWorker from './serviceWorker';
 
