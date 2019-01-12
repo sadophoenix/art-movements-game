c9413a5a8f7022f87fe94e7c2e81a06a20203e41
David Santogidis <s.david19193@gmail.com>
2019-01-12 12:04:33 +0100

    Added game component and menu

---
+++ src/Game.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Puzzle from './Puzzle';
+import './Game.css'
+
+const images = [
+    'https://i.pinimg.com/236x/71/d0/f0/71d0f02081ef18a9b22675c2aeeb324c--georges-braque-pablo-picasso.jpg',
+    'https://i.pinimg.com/originals/1a/e1/b9/1ae1b99c49475dc8b1fde6eade24bb91.jpg'
+];
+
+class Game extends React.Component {
+    states = {menu: "menu", puzzle: "puzzle"};
+
+    constructor(props) {
+        super(props);
+        this.state = {page: this.states.menu};
+    }
+
+    mainMenu() {
+        return  <div className={"menu-div"} >
+                    <ul>
+                        <li> <button onClick={() => {this.setState({ page: this.states.puzzle })}} type="button"> start </button> </li>
+                        <li> <button type="button"> tutorial </button> </li>
+                    </ul>
+                </div>;
+    }
+
+    puzzleGame() {
+        return <div>
+            <Puzzle image={images[0]} image2={images[1]} />
+                <button onClick={() => {this.setState({ page: this.states.menu })}} type="button"> Back to menu </button>
+        </div>
+    }
+
+    render() {
+        const { page } = this.state;
+        switch (page) {
+            case "menu":
+                return this.mainMenu();
+            case "puzzle":
+                return this.puzzleGame();
+        }
+    }
+}
+
+export default Game;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Puzzle from './Puzzle';
+import Game from './Game';
 import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-
 ReactDOM.render(
-    <Puzzle
-        image='https://i.pinimg.com/236x/71/d0/f0/71d0f02081ef18a9b22675c2aeeb324c--georges-braque-pablo-picasso.jpg'
-        image2='https://i.pinimg.com/originals/1a/e1/b9/1ae1b99c49475dc8b1fde6eade24bb91.jpg'
-    />,
+    <Game/>,
     document.getElementById('root')
 );
 
